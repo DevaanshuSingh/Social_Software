d69996c74fea99e8660ee502c8630a1c96fa1d96
DevaanshuSingh <devaanshu111@gmail.com>
2024-12-28 02:11:29 +0530

    Some More,

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 
 try {
-    if (isset($_POST['full-name'], $_POST['user-name'], $_POST['user-password'], $_POST['location'], $_POST['dob'], $_POST['bio'], $_POST['intrests'], $_FILES['image'])) {
+    if (isset($_POST['full-name'], $_POST['user-name'], $_POST['user-password'], $_POST['location'], $_POST['dob'], $_POST['bio'], $_POST['intrests'],$_POST['email'], $_FILES['image'])) {
         $fullName = $_POST['full-name'];
         $userName = $_POST['user-name'];
         $userPassword = $_POST['user-password'];
@@ -10,6 +10,7 @@ try {
         $dob = $_POST['dob'];
         $bio = $_POST['bio'];
         $intrests = $_POST['intrests'];
+        $email = $_POST['email'];
 
         $imagePath = null;
         if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
@@ -44,15 +45,18 @@ try {
             echo "$key: $posted<br>";
         }
 
-        $stmt = $pdo->prepare("INSERT INTO users (full_name, username, bio, location, dob, interests, profile_picture) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt = $pdo->prepare("INSERT INTO users (fullName, userName, userPassword, bio, location, dob, interests, profile_picture,email) VALUES (?, ?, ?, ?, ?, ?, ?,?,?)");
 
-        if ($stmt->execute([$fullName, $userName, $bio, $location, $dob, $intrests, $imagePath])) {
+        if ($stmt->execute([$fullName, $userName, $userPassword, $bio, $location, $dob, $intrests, $imagePath,$email])) {
             echo "<script>alert('Registration successful')<script>";
         } else {
             echo "<script>alert('Not Regestered')<script>";
         }
     } else {
-        die("Required data not provided.");
+        echo '<script>
+        alert("Required data not provided.");
+        window.location.href = "index.php";
+        </script>';
     }
 } catch (PDOException $e) {
     echo "Error While Registering:<br>" . $e->getMessage();
